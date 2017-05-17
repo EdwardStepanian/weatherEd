a16eb06a9074f2fe14c54bdafa2bbfd0ff73b4d3
Edward <edward.stepanian@astgeek.com>
2017-05-17 17:23:50 +0400

    some fixes

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import './App.css';
 import WeatherDisplay from './WeatherDisplay';
 import "bootswatch/superhero/bootstrap.css";
 import {Navbar, NavItem, Nav, Grid, Row, Col} from "react-bootstrap";
-
 const PLACES = [
-    {name: "Palo Alto", zip: "94303"},
-    {name: "San Jose", zip: "94088"},
-    {name: "Santa Cruz", zip: "95062"},
-    {name: "Honolulu", zip: "96803"}
+    {name: "Yerevan",zip:"0002"},
+    {name: "Artashat", zip: "174979"},
+    {name: "Vanadzor", zip: "616530"},
+    {name: "Hrazdan",zip:"616629"},
+    {name: "Agarak",zip:"175007"},
+    {name: "Kapan",zip:"174875"},
+    {name: "Goris",zip:"174895"},
+    {name: "Metsamor", zip: '616417'}
+
 ];
+
 
 class App extends Component {
 
@@ -25,35 +30,44 @@ class App extends Component {
         const activePlace = this.state.activePlace;
         return (
             <div className="App">
-                    <Navbar>
-                        <Navbar.Header>
-                            <Navbar.Brand>
-                                React Simple Weather App
-                            </Navbar.Brand>
-                        </Navbar.Header>
-                    </Navbar>
-                    <Grid>
-                        <Row>
-                            <Col md={4} sm={4}>
-                                <h3>Select a city</h3>
-                                <Nav
-                                    bsStyle="pills"
-                                    stacked
-                                    activeKey={activePlace}
-                                    onSelect={index => {
-                                        this.setState({activePlace: index});
-                                    }}
-                                >
-                                    {PLACES.map((place, index) => (
-                                        <NavItem key={index} eventKey={index}>{place.name}</NavItem>
-                                    ))}
-                                </Nav>
-                            </Col>
-                            <Col md={8} sm={8}>
-                                <WeatherDisplay key={activePlace} zip={PLACES[activePlace].zip}/>
-                            </Col>
-                        </Row>
-                    </Grid>
+                <Navbar>
+                    <Navbar.Header>
+                        <Navbar.Brand>
+                            WeatherEd
+                        </Navbar.Brand>
+                    </Navbar.Header>
+                </Navbar>
+                <Grid>
+                    <Row>
+                        <Col md={4} sm={4}>
+                            <h3>Select a city</h3>
+                            <Nav
+                                bsStyle="pills"
+                                stacked
+                                activeKey={activePlace}
+                                onSelect={index => {
+                                    this.setState({activePlace: index});
+                                }}
+                            >
+                                {PLACES.map((place, index) => (
+                                    <NavItem key={index} eventKey={index}>{place.name}</NavItem>
+                                ))}
+                            </Nav>
+                        </Col>
+                        <Col md={8} sm={8}>
+                            <WeatherDisplay key={activePlace} zip={PLACES[activePlace].zip} place={PLACES[activePlace].name}/>
+                        </Col>
+                    </Row>
+                </Grid>
+                <div className="copyCont">
+                    <div className="copy">Copyrights © Edward Stepanian . All Rights Reserved</div>
+                    <div className="git">
+                        <img src="https://cdn2.iconfinder.com/data/icons/social-icons-33/128/Github-128.png" alt="github " />
+
+                        <a href="https://github.com/EdwardStepanian/GalleryEd">GitHub repo</a>
+                    </div>
+                </div>
+
             </div>
         );
     }
--- src/WeatherDisplay.jsx
+++ src/WeatherDisplay.jsx
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 
-
 class WeatherDisplay extends Component {
 	constructor() {
 		super();
@@ -10,27 +9,30 @@ class WeatherDisplay extends Component {
 	}
 	componentDidMount() {
 		const zip = this.props.zip;
-		const URL = `http://api.openweathermap.org/data/2.5/weather?q=${zip}&appid=b1b35bba8b434a28a0be2a3e1071ae5b&units=imperial`;
+		const place = this.props.place;
+		console.log(this.props)
+		const URL = `http://api.openweathermap.org/data/2.5/weather?q=${place}&appid=b1b35bba8b434a28a0be2a3e1071ae5b&units=imperial`;
 		fetch(URL).then(res => res.json()).then(json => {
-			console.log(json);
 			this.setState({ weatherData: json });
 		});
 	}
 	render() {
 		const weatherData = this.state.weatherData;
-		if (!weatherData) return <div>Loading</div>;
+		if (!weatherData) return <div>Loading...</div>;
 		const weather = weatherData.weather[0];
 		const iconUrl = "http://openweathermap.org/img/w/" + weather.icon + ".png";
+		let weatherTempCelsius = (weatherData.main.temp - 32)*5/9;
+		let weatherSpeed =weatherData.wind.speed*0.44704;
 		return (
 			<div>
 				<h1>
 					{weather.main} in {weatherData.name}
 					<img src={iconUrl} alt={weatherData.description} />
 				</h1>
-				<p>Current: {weatherData.main.temp}°</p>
-				<p>High: {weatherData.main.temp_max}°</p>
-				<p>Low: {weatherData.main.temp_min}°</p>
-				<p>Wind Speed: {weatherData.wind.speed} mi/hr</p>
+				<p>Current: {weatherTempCelsius.toFixed(1)}°</p>
+				<p>Wind Speed: {weatherSpeed.toFixed(1)} m/sec</p>
+				<p>Humidity: {weatherData.main.humidity}%</p>
+				<footerGrid />
 			</div>
 		);
 	}
